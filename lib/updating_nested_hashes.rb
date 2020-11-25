868fcaf6e98820071aebbf0891265a65a17a6559
charming-input-2976 <mbingramm@gmail.com>
2020-11-25 19:06:22 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,14 +20,10 @@ def update_status
       }
    }
 
- 
-
-
-
-
-  # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
   epic_tragedy
 end
+
+
 
 def add_characters
    epic_tragedy = {
@@ -48,7 +44,11 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
+      },
+      :additional_characters => [
+        {name: "Prince Escalus"},
+        {name: "Apothecary"}
+        ]
    }
 
    # Write code below that does the following programmatically:
